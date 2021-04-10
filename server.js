6ec58f4678a7be53d659a66173b1e90ba401adfc
gautism1 <vipingautam.m@gmail.com>
2021-04-10 13:51:31 +0530

    updated node version2

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express=require('express');
 const path = require("path");
 const helmet = require("helmet");
 const morgan =require('morgan');
-const config=require("config")
 const routes = require('./routes')
 const app=express();
 let cors = require('cors');
